0e36c8cb44465144253e135ab5bd0a30da5a29b3
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-04-05 09:43:37 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 田中宏貴 on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
